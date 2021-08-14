7a3aae43284469ac1bfc190a9d377092361e3fed
toshiki-nakamura-git <88643462+toshiki-nakamura-git@users.noreply.github.com>
2021-08-14 21:36:32 +0900

    Update face-recg-0801.py

--- face-recg-0801.py
+++ face-recg-0801.py
@@ -11,7 +11,7 @@ face_api_url = "https://facetest0808.cognitiveservices.azure.com/face/v1.0/detec
 
 #
 st.title('顔認識アプリ')
-st.write('アップロードされた画像の顔を認識し、推定年齢と幸福度合い（0.0 ～ 最大値1.0）を表示するアプリです。')
+st.write('アップロードされた画像の顔を認識し、推定年齢とハッピーレベル（0.0 ～ 最大値1.0）を表示するアプリです。')
 uploaded_file = st.file_uploader("choose an Image...",type ="jpg")
 
 if uploaded_file is not None:
@@ -41,6 +41,7 @@ if uploaded_file is not None:
         draw.text((rect['left']+10,rect['top']+10),"AGE : "+str(age_test),font=font)
         draw.text((rect['left']+10,rect['top']+rect['height']+10),"Happy Level : "+str(emo_test))
         st.write('推定年齢は、',str(age_test),'歳です！')
+        st.write('推定ハッピーレベルは、',str(emo_test),'歳です！')
 
     st.image(img,caption='uploded image',use_column_width=True)
    
