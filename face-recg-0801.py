e08b3ea8f9991755fcc45ce4f6056641c317d9fd
toshiki-nakamura-git <88643462+toshiki-nakamura-git@users.noreply.github.com>
2021-08-14 20:38:17 +0900

    Update face-recg-0801.py

--- face-recg-0801.py
+++ face-recg-0801.py
@@ -28,7 +28,7 @@ if uploaded_file is not None:
         'returnFaceAttributes':'age,gender,headPose,emotion,smile,hair,makeup,glasses'
         }
 
-#    res = requests.post(face_api_url, params=params,headers=headers, data=binary_img)
+    res = requests.post(face_api_url, params=params,headers=headers, data=binary_img)
 #    results = res.json()
 #    fnt = ImageFont.truetype("arial.ttf", 20)
 #
