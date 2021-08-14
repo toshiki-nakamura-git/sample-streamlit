5da093df6e11604f5ec0d0935c48e6deb47341ac
toshiki-nakamura-git <88643462+toshiki-nakamura-git@users.noreply.github.com>
2021-08-14 21:12:53 +0900

    Update face-recg-0801.py

--- face-recg-0801.py
+++ face-recg-0801.py
@@ -37,7 +37,7 @@ if uploaded_file is not None:
         emo_test = result['faceAttributes']['emotion']['happiness']
         draw = ImageDraw.Draw(img)
         draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'],rect['top']+rect['height'])], fill=None, outline = 'green', width =5)
-        draw.text((rect['left']+10,rect['top']+10),"AGE : "+str(age_test),font=ImageFont.truetype("arial.ttf", 15))
+        draw.text((rect['left']+10,rect['top']+10),"AGE : "+str(age_test))
         draw.text((rect['left']+10,rect['top']+rect['height']+10),"Happy Level : "+str(emo_test))
 
     st.image(img,caption='uploded image',use_column_width=True)
